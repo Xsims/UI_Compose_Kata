9721d6582dcdf2616b15446d82b87d62c0631634
Xsims <romeo.prosecco@gmail.com>
2024-05-14 09:19:10 +0200

    Update the code to reduce the number of modifier methods used by one

--- app/src/main/java/com/xsims/uicomposekata/MainActivity.kt
+++ app/src/main/java/com/xsims/uicomposekata/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
@@ -25,14 +24,14 @@ private fun MyScreen() {
         MyCard {
             Box(
                 modifier = Modifier
-                    .fillMaxSize()
+                    .fillMaxWidth()
                     .weight(1f),
                 propagateMinConstraints = true
             ) {
                 CoverImage()
                 Box(
                     modifier = Modifier
-                        .fillMaxSize()
+                        .fillMaxWidth()
                         .align(Alignment.TopEnd)
                         .padding(8.dp),
                 ) {
